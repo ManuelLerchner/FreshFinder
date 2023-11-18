8d035b19bd1738aa77939b709c780523564474c9
EmanuelMedinaAraujo <60936205+EmanuelMedinaAraujo@users.noreply.github.com>
2023-11-18 21:12:58 +0100

    added sync

--- apps/web/src/pages/StartSession.tsx
+++ apps/web/src/pages/StartSession.tsx
@@ -1,42 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { supabase } from "../components/SupabaseClient";
+import { localUser, supabase } from "../components/SupabaseClient";
+import { RealtimeChannel } from "@supabase/supabase-js";
 
 
 export default function StartSession() {
-  const navigate = useNavigate();
+  // Simple function to log any messages we receive
+  function adaptClientData( payload: any) {
+   const {uuid, hardPreference, userPreferences} = payload.payload.localUserData
+   console.log(payload)
+  }
+
   return (
     <div className="flex flex-col items-center my-4 gap-10">
       <h1 className="text-3xl font-bold">StartSessions</h1>
-
       <p className="text-lg">Start Session!</p>
       <button
         className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-md hover:scale-[102%]"
         onClick={() => {
-          const roomOne = supabase.channel('room1') // set your topic here
-          // navigate("/meal-picker");
+          if(supabase.getChannels().length > 0) {
+            console.log("Already in a session, leaving session");
+            // wait until we are unsubscribed
+            supabase.channel('room-1').unsubscribe()
+          }
+          const channelA = supabase.channel('room-1')
+          // Subscribe to the Channel
+          channelA
+            .on(
+              'broadcast',
+              { event: 'test' },
+              (payload) => adaptClientData(payload)
+            )
+            .subscribe()
+          console.log("Session Started");
         }}
       >
         Start Session
       </button>
-      <p className="text-lg">Join Session!</p>
+
+      <p className="text-lg">SendMessage!</p>
       <button
         className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-md hover:scale-[102%]"
         onClick={() => {
-          const channel = supabase.channel('room1')
-          channel
-            .on('presence', { event: 'sync' }, () => {
-              console.log('Synced presence state: ', channel.presenceState())
-            })
-            .subscribe(async (status) => {
-              if (status === 'SUBSCRIBED') {
-                await channel.track({ online_at: new Date().toISOString() })
-              }
-            })
-          // navigate("/meal-picker");
+          const channelB = supabase.channel('room-1')
+          channelB.subscribe((status) => {
+            if (status !== 'SUBSCRIBED') {
+              return null;
+            }
+            channelB.send({
+              type: 'broadcast',
+              event: 'test',
+              payload: { localUserData: localUser },
+            });
+          });
+          channelB.on(
+            'broadcast',
+            { event: 'test' },
+            (payload) => adaptClientData(payload)
+          )
         }}
       >
-        Join Session!
+        Send Message
       </button>
     </div>
   );
