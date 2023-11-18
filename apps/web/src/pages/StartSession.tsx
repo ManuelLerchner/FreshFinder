c616cc35dc0e1979dbd1c21373b58b9a2fee9090
EmanuelMedinaAraujo <60936205+EmanuelMedinaAraujo@users.noreply.github.com>
2023-11-18 18:46:35 +0100

    Temp changes

--- apps/web/src/pages/StartSession.tsx
+++ apps/web/src/pages/StartSession.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { supabase } from "../components/SupabaseClient";
+
 
 export default function StartSession() {
   const navigate = useNavigate();
@@ -11,6 +13,7 @@ export default function StartSession() {
       <button
         className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-md hover:scale-[102%]"
         onClick={() => {
+          const roomOne = supabase.channel('room1') // set your topic here
           // navigate("/meal-picker");
         }}
       >
@@ -20,6 +23,16 @@ export default function StartSession() {
       <button
         className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-md hover:scale-[102%]"
         onClick={() => {
+          const channel = supabase.channel('room1')
+          channel
+            .on('presence', { event: 'sync' }, () => {
+              console.log('Synced presence state: ', channel.presenceState())
+            })
+            .subscribe(async (status) => {
+              if (status === 'SUBSCRIBED') {
+                await channel.track({ online_at: new Date().toISOString() })
+              }
+            })
           // navigate("/meal-picker");
         }}
       >
