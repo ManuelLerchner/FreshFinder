8d972f806d4de62e499abe4d22c812ef945adfd1
ManuelLerchner <manuel.lerchner1111@gmail.com>
2023-11-18 17:41:28 +0100

    Update tags in recommendation algorithm

--- apps/web/src/pages/SelectionPage.tsx
+++ apps/web/src/pages/SelectionPage.tsx
@@ -4,26 +4,52 @@ import RecommendedFood from "../components/RecommendedFood";
 import { mergePreferences } from "../util/updatePreferences";
 import TagCard from "../components/TagCard";
 import { useNavigate } from "react-router-dom";
-
-const questionsDB = [
-  {
-    question: "Do you like meat?",
-    imageUri:
-      "https://images.squarespace-cdn.com/content/v1/5ba526da8dfc8c614b5e5cc6/1540594362409-CXGHX9PLATQYS922IKGN/Products-1048-Fotolia_172144922_M.jpg?format=2500w",
-    categories: ["meat"],
-  },
-  {
-    question: "Do you like salad?",
-    imageUri:
-      "https://images.squarespace-cdn.com/content/v1/5ba526da8dfc8c614b5e5cc6/1540594362409-CXGHX9PLATQYS922IKGN/Products-1048-Fotolia_172144922_M.jpg?format=2500w",
-    categories: ["vegan"],
-  },
-];
+import { requestRecommendations } from "../algorithms/recommendation";
+import { localUser, supabase } from "../components/SupabaseClient";
 
 export default function SelectionPage() {
   const [userPreferences, setUserPreferences] = useState<{
     [key: string]: number;
   }>({});
+
+  const [questions, setQuestions] = useState<
+    { question: string; imageUri: string; categories: string[] }[]
+  >([]);
+
+  const [recommendedFood, setRecommendedFood] = useState<{
+    title: string;
+    url: string;
+    tags: string[];
+  } | null>(null);
+
+  useEffect(() => {
+    const queryFunction = async () => {
+      const { recomended_food, next_tags } = await requestRecommendations(
+        localUser.hardPreferences,
+        userPreferences
+      );
+
+      setRecommendedFood(recomended_food);
+
+      const { data: next_questions } = await supabase
+        .from("Tags")
+        .select("id, category, image_url")
+        .in("category", next_tags)
+        .select();
+
+      if (!next_questions) return;
+
+      const question = next_questions.map((question) => ({
+        question: question.category,
+        imageUri: question.image_url,
+        categories: [question.category],
+      }));
+
+      setQuestions(question);
+    };
+
+    queryFunction();
+  }, [userPreferences]);
 
   const navigator = useNavigate();
 
@@ -35,10 +61,10 @@ export default function SelectionPage() {
     <div className="flex flex-col items-center justify-center my-4 h-full">
       <div className="flex flex-col items-center justify-center gap-4">
         <div className="relative flex h-96 w-full justify-center items-center">
-          {questionsDB.map((question) => (
+          {questions.map((question) => (
             <QuestionCard
               key={question.question}
-              question={question.question}
+              question={"Do you like " + question.question + " food?"}
               imageUri={question.imageUri}
               onPositive={() => {
                 const updates: { [key: string]: number } = {};
@@ -72,21 +98,25 @@ export default function SelectionPage() {
 
         <h2 className="text-2xl font-bold my-4 self-start">Recommended Food</h2>
 
-        <RecommendedFood
-          name="Chicken"
-          image="https://ais.kochbar.de/kbrezept/22629_1009908/1200x1200/haehnchen-in-curry-sahne-sosse-auf-reis-rezept-bild-nr-2.jpg"
-          tags={["vegan", "low-carb", "meat", "salad"]}
-          onDiscard={() => {}}
-        />
+        {recommendedFood && (
+          <>
+            <RecommendedFood
+              name={recommendedFood.title}
+              image={recommendedFood.url}
+              tags={recommendedFood.tags}
+              onDiscard={() => {}}
+            />
 
-        <button
-          className="bg-green-500 text-white px-4 py-2 rounded-lg hover:scale-[102%] w-full"
-          onClick={() => {
-            navigator("/success");
-          }}
-        >
-          Order recommended food
-        </button>
+            <button
+              className="bg-green-500 text-white px-4 py-2 rounded-lg hover:scale-[102%] w-full"
+              onClick={() => {
+                navigator("/success");
+              }}
+            >
+              Order recommended food
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
