d20e12d5a09fbc2c63d5c83c67e0d8eaec30a7a9
EmanuelMedinaAraujo <60936205+EmanuelMedinaAraujo@users.noreply.github.com>
2023-11-18 16:59:00 +0100

    Fixed navigation

--- apps/web/src/pages/Configure.tsx
+++ apps/web/src/pages/Configure.tsx
@@ -64,7 +64,7 @@ export default function Configure() {
               console.log(selected);
             }}
             onNext={() => {
-              navigate("/");
+              navigate("/meal-picker");
             }}
           />
         )}
