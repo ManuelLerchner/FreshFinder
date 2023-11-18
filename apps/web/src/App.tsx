830d608fe4c3d47dc87631720963103219a4076f
Emanuel Medina <emanuel.andre@web.de>
2023-11-18 04:33:01 +0100

    Connected supabase and fixed bugs

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,7 +3,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import MealPicker from "./pages/MealPicker";
 import Configure from "./pages/Configure";
-import Login from "./components/Login";
+import Login from "./pages/Login";
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient("https://qtribxnypykjlyjswlno.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0cmlieG55cHlramx5anN3bG5vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAyNjk4MjQsImV4cCI6MjAxNTg0NTgyNH0.14IgLqK90KonabGozsinPI29MkLWBtkWCffC3NJcSMw")
 
 function App() {
   return (
@@ -14,7 +18,7 @@ function App() {
           <Route path="/configure" element={<Configure />} />
         </Route>
         <Route path="/auth" element={<MainLayout />}>
-          <Route path="auth/login" element={<Login />} />
+          <Route path="login" element={<Login />} />
         </Route>
       </Routes>
     </BrowserRouter>
